a4195bc763b22053f558038b1fa6eb05e900aca8
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-11-13 12:14:09 -0300

    add new version to odoo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Exe Automated Collection Tracking',
-    'version': '17.0',
+    'version': '18.0',
     'category': 'Tools',
     'author':'Mauro Bogado,Exemax',
     'summary': 'Modulo para el seguimiento automatizado de cobranzas',
